05f7cc9f1c0ccbaa76bff3a78a1bba6ba302dd15
Chenfei <ychjixian@163.com>
2020-04-23 23:21:45 +0800

    关于我们 加入我们

--- bootstrap/js/about.js
+++ bootstrap/js/about.js
@@ -1,4 +1,36 @@
 $(document).ready(function() {
+    handleClickEvent();
+    initPageText();
+
+    function handleClickEvent() {
+        $("#chinese").click(function() {
+            localStorage.setItem('language', 'cn');
+            initPageText();
+        });
+        $("#english").click(function() {
+            localStorage.setItem('language', 'en');
+            initPageText();
+        });
+    }
+
+    function initPageText() {
+        var language = localStorage.getItem('language');
+        if (language == 'en' || language == null) {
+            // 4-23
+            $("#about_kiwi").html(EN.about_kiwi)
+            $("#aboutus_intro_one").html(EN.aboutus_intro_one)
+            $("#aboutus_intro_two").html(EN.aboutus_intro_two)
+            $("#aboutus_intro_three").html(EN.aboutus_intro_three)
+            $("#aboutus_intro_four").html(EN.aboutus_intro_four)
+        } else if (language == 'cn') {
+            // 4-23
+            $("#about_kiwi").html(CN.about_kiwi)
+            $("#aboutus_intro_one").html(CN.aboutus_intro_one)
+            $("#aboutus_intro_two").html(CN.aboutus_intro_two)
+            $("#aboutus_intro_three").html(CN.aboutus_intro_three)
+            $("#aboutus_intro_four").html(CN.aboutus_intro_four)
+        }
+    }
     // 订阅
     $("#subscribeBtn").click(function(e) {
         $("#userInfoBox").toggle();
--- bootstrap/js/contact.js
+++ bootstrap/js/contact.js
@@ -1,4 +1,36 @@
 $(document).ready(function() {
+    handleClickEvent();
+    initPageText();
+
+    function handleClickEvent() {
+        $("#chinese").click(function() {
+            localStorage.setItem('language', 'cn');
+            initPageText();
+        });
+        $("#english").click(function() {
+            localStorage.setItem('language', 'en');
+            initPageText();
+        });
+    }
+
+    function initPageText() {
+        var language = localStorage.getItem('language');
+        if (language == 'en' || language == null) {
+            // 4-23
+            $("#joinus").html(EN.joinus)
+            $("#join_intro_one").html(EN.join_intro_one)
+            $("#join_intro_two").html(EN.join_intro_two)
+            $("#future_two").html(EN.future_two)
+            $("#future_three").html(EN.future_three)
+        } else if (language == 'cn') {
+            // 4-23
+            $("#bannerIntro").html(CN.compant_intro)
+            $("#kiwilab_content").html(CN.kiwilab_content)
+            $("#future_one").html(CN.future_one)
+            $("#future_two").html(CN.future_two)
+            $("#future_three").html(CN.future_three)
+        }
+    }
     // 订阅
     $("#subscribeBtn").click(function(e) {
         $("#userInfoBox").toggle();
